2e6c4e978f96475b588bedaa8a3e3df993f9cf37
deejkelms <dkelmanson@gmail.com>
2016-05-22 16:13:38 +0000

    added contact for saving plus flash notifications

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,18 @@ class ContactsController < ApplicationController
   end
   
   def create
-    
+    @contact = Contact.new(contact_params)
+      if @contact.save
+        flash[:success] = "New User Created!"
+        redirect_to new_contact_path
+      else
+        flash[:danger] = "Not enough information :("
+        redirect_to new_contact_path
+      end
   end
   
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
