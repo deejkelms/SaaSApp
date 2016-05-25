46d973d23e10c59e7da068ca8af6c6f6795fffef
deejkelms <dkelmanson@gmail.com>
2016-05-25 14:15:15 +0000

    added link to plans 1 and 2

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
 
-	def home
-	end
+  def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
+  end
 
-	def about
+  def about
   end
 	
 end
